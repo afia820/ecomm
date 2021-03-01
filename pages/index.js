78419ffbf69aa7a13db9b61516c8f5b74df55c75
Afia Siaw <as820@aol.com>
2021-02-28 20:44:31 -0500

    added styling to products

--- pages/index.js
+++ pages/index.js
@@ -16,59 +16,47 @@ export default function Home() {
 
         <p className={styles.description}>The place to get some skates!</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <img src="/images/jungle.png" alt="Moxi Jungle Skates" />
-            <h3>Moxi Jungle Roller Skate</h3>
-            <p>
-              The Moxi Jungle Skates are brown leopard print vinyl skates, vegan
-              friendly, and ready to roll.
-            </p>
-          </a>
+        <ul className={styles.grid}>
+          <li className={styles.card}>
+            <a href="https://nextjs.org/docs">
+              <img src="/images/jungle.png" alt="Moxi Jungle Skates" />
+              <h3>Moxi Jungle Roller Skate</h3>
+              <p>
+                The Moxi Jungle Skates are brown leopard print vinyl skates,
+                vegan friendly, and ready to roll.
+              </p>
+            </a>
+          </li>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <img
-              src="/images/rainbow-rider.png"
-              alt="Moxi Rainbow Rider Skates"
-            />
-            <h3>Rainbow Rider</h3>
-            <p>
-              The Moxi Rainbow Rider is designed for the beginning recreational
-              skater looking for an outdoor roller skate packed with
-              high-quality comfort at an affordable price.
-            </p>
-          </a>
+          <li className={styles.card}>
+            <a href="https://github.com/vercel/next.js/tree/master/examples">
+              <img
+                src="/images/watermelon.png"
+                alt="Beach Bunny Watermelon Skates"
+              />
+              <h3>Beach Bunny Roller Skates - Watermelon</h3>
+              <p>
+                Features a traditional high top boot including drum-dyed vinyl
+                uppers and additional ankle padding that provides comfort and
+                security.
+              </p>
+            </a>
+          </li>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <img
-              src="/images/watermelon.png"
-              alt="Beach Bunny Watermelon Skates"
-            />
-            <h3>Beach Bunny Roller Skates - Watermelon</h3>
-            <p>
-              The Beach Bunny features a traditional high top boot with
-              thoughtful touches including drum-dyed vinyl uppers for more
-              authentic color saturation and additional ankle padding that
-              provides comfort and security whether you’re cruising down the
-              boulevard or ripping up your favorite skate park.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <img src="/images/black-suede.png" alt="Lolly Black Suede Skates" />
-            <h3>Lolly Outdoor Complete - Classic Black Suede</h3>
-            <p>
-              Moxi Lolly Black Skates are a mid-range lifestyle skate, designed
-              for recreation and street skating.
-            </p>
-          </a>
-        </div>
+          <li className={styles.card}>
+            <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+              <img
+                src="/images/black-suede.png"
+                alt="Lolly Black Suede Skates"
+              />
+              <h3>Lolly Outdoor Complete - Classic Black Suede</h3>
+              <p>
+                Moxi Lolly Black Skates are a mid-range lifestyle skate,
+                designed for recreation and street skating.
+              </p>
+            </a>
+          </li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
